0f11e58d8f74089439f29492a0932a12a0c1fc32
jmarcosdias <jmsdias6@gmail.com>
2022-07-11 21:57:48 +0000

    Customise mobile top header

--- boutique_ado/settings.py
+++ boutique_ado/settings.py
@@ -157,6 +157,7 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
 
+# Where all uploaded media files will go
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
