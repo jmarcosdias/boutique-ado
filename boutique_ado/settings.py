0cec63ab965310d3dd85d7a0b26af1d8413cbc6d
jmarcosdias <jmsdias6@gmail.com>
2022-08-08 10:56:38 +0000

    Add local to trusted origins

--- boutique_ado/settings.py
+++ boutique_ado/settings.py
@@ -217,3 +217,8 @@ else:
     EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
     EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASS')
     DEFAULT_FROM_EMAIL = os.environ.get('EMAIL_HOST_USER')
+
+
+CSRF_TRUSTED_ORIGINS = [
+    'https://8000-jmarcosdias-boutiqueado-ybj32mjbbql.ws-eu59.gitpod.io'
+]
